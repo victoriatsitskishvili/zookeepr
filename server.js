99acbd6691e762c6f4cd6a9c8d36bc78d9a75429
victoriatsitskishvili <victoria.tsitskishvili@gmail.com>
2021-12-26 17:01:59 -0500

    adding a second route and error

--- server.js
+++ server.js
@@ -50,6 +50,20 @@ app.get('/api/animals', (req, res) => {
   res.json(results);
 });
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
+
+app.get('/api/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
